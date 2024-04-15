06c16d95151f7f59dbc1347228c6e599ffd7df31
thgMatajs <tprmatajs@gmail.com>
2024-04-15 19:55:08 -0300

    create splash screen and change icon app

---
+++ app/src/main/kotlin/com/gentalha/moviechallenge/ui/viewmodel/SplashScreenViewModel.kt
@@ -0,0 +1,24 @@
+package com.gentalha.moviechallenge.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashScreenViewModel @Inject constructor() : ViewModel() {
+
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(1000L)
+            _isReady.value = true
+        }
+    }
+}
